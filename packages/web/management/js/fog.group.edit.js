08827c3dca45885050f42ac755d1991c061b6aee
Jbob <contact@jbob.io>
2014-08-01 14:55:02 -0500

    Make the installer remove 'FOG Service' service, and rename the 'FOG Service' folder to 'FOGService'

---
+++ packages/web/management/js/fog.group.edit.js
@@ -0,0 +1,71 @@
+/****************************************************
+ * FOG Group Management - Edit - JavaScript
+ *	Author:		Blackout
+ *	Created:	10:26 AM 1/01/2012
+ *	Revision:	$Revision$
+ *	Last Update:	$LastChangedDate$
+ ***/
+
+$(function()
+{
+	// Just hide the group info
+	$('#hostNoGroup').hide();
+	// Bind to AD Settings checkbox
+	$('#adEnabled').change(function() {
+		
+		if ( $(this).attr('checked') )
+		{	
+			if ( $('#adDomain').val() == '' && $('#adUsername').val() == '' &&  $('#adPassword').val() == '')
+			{
+				$.ajax({
+					'type':		'GET',
+					'url':		'ajax/host.adsettings.php',
+					'cache':	false,
+					'dataType':	'json',
+					'success':	function(data)
+					{	
+						$('#adDomain').val(data['domainname']);
+						$('#adUsername').val(data['domainuser']);
+						$('#adPassword').val(data['domainpass']);
+					}
+				});
+			}
+			if ($('#adOU').is('input:text') && $('#adOU').val() == '')
+			{
+				$.ajax({
+					'type': 'GET',
+					'url': 'ajax/host.adsettings.php',
+					'cache': false,
+					'dataType': 'json',
+					'success': function(data)
+					{
+						$('#adOU').val(data['ou']);
+					}
+				});
+			}
+		}
+	});
+	// Show hide based on checked state.
+	$('#hostNoShow').change(function() {
+		if ($(this).attr('checked'))
+		{
+			$('#hostNoGroup').show();
+		}
+		else
+		{
+			$('#hostNoGroup').hide();
+		}
+	});
+	
+	// Host Tasks - show advanced tasks on click
+	$('.advanced-tasks-link').click(function()
+	{
+		$(this).parents('tr').toggle('fast', function()
+		{
+			$('#advanced-tasks').toggle('slow');
+		});
+		$(this).parents('tr').toggle('fast');
+		
+		//event.preventDefault();
+	});
+});
---
+++ packages/web/service/hostname.php
@@ -0,0 +1,31 @@
+<?php
+require_once('../commons/base.inc.php');
+try
+{
+	$HostManager = new HostManager();
+	$MACs = HostManager::parseMacList($_REQUEST['mac']);
+	if (!$MACs)
+		throw new Exception('#!im');
+	// Get the Host
+	$Host = $HostManager->getHostByMacAddresses($MACs);
+	if (!$Host->isValid())
+		throw new Exception('#!nf');
+	if (!$Host->isHostnameSafe())
+		throw new Exception('#!ih');
+	// Send the information.
+	print '#!ok='.$Host->get('name')."\n";
+	print '#AD='.$Host->get('useAD')."\n";
+	print '#ADDom='.$Host->get('ADDomain')."\n";
+	print '#ADOU='.$Host->get('ADOU')."\n";
+	print '#ADUser='.$Host->get('ADDomain').'\\'.$Host->get('ADUser')."\n";
+	print '#ADPass='.$Host->get('ADPass');
+	if (trim(base64_decode($Host->get('productKey'))))
+		print "\n#Key=".base64_decode($Host->get('productKey'));
+	// Just inform the user (probably not needed and probably won't display.)
+	if (!$Host->get('useAD'))
+		throw new Exception("#!er: Join domain disabled on this host.\n");
+}
+catch (Exception $e)
+{
+	print $e->getMessage();
+}
